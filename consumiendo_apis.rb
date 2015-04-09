dd37e0dce947121664d33c37142d6fff2a5b52ce
samuel melgarejo <samuel_021093@hotmail.com>
2015-04-09 14:51:48 -0430

    class feed,noticia

--- consumiendo_apis.rb
+++ consumiendo_apis.rb
@@ -1,60 +1,143 @@
 require 'httparty'
+require 'colorize'
 
-class Mashable
+class Feed
+
+	def news type
+
+		if type == "Mashable"
+			m = Mashable.new 
+			aux = m.noticia
+			return aux
+		elsif type == "Reddit"
+			r = Reddit.new
+			aux = r.noticia
+			return aux
+		elsif type == "Digg"
+			d = Digg.new
+			aux = d.noticia
+			return aux
+		end		
+	end
+end	
+
+class Notice
+
+	attr_accessor :title, :author, :date, :link, :type
+
+	def initialize title, author, date, link, type
+
+		@title = title
+		@author = author
+		@date = date
+		@link = link
+    @type = type
+
+	end	
+end	
+
+class Mashable 
 	include HTTParty
 	# base_uri  'http://mashable.com/stories.json'
 
-	def notices
-		news = HTTParty.get("http://mashable.com/stories.json?limit=5")
+	def noticia
+		not_mas = []
+		news = HTTParty.get("http://mashable.com/stories.json")
 		news["rising"].each do |notice|
-    		puts "#{notice["author"]}"
-    		puts "#{notice["title"]}"
-    		puts "#{notice["link"]}"
-    		puts "#{notice["post_date"]}"
-    		puts "==================M"
+    	  title =  "#{notice["title"]}"
+    	  author =  "#{notice["author"]}"
+    	  link =  "#{notice["link"]}"
+    	  date =  Time.at(("#{notice["post_date"]}")
+        type = "Mashable"
+    	  notice = Notice.new(title, author, link, date, type)
+    	  not_mas.push(notice)
+    		# "==================M"
 		end
+
+		return not_mas
 	end	
 end	
 
-class Reddit
+class Reddit 
 	include HTTParty
 
-	def notices
-		news = HTTParty.get("http://www.reddit.com/.json?limit=5")
-		news["data"]["children"].each do |notice|
-    		puts "#{notice["data"]["title"]}"
-    		puts "#{notice["data"]["author"]}"
-    		puts "#{notice["data"]["url"]}"
-    		puts "#{notice["data"]["created"]}"
-    		puts "=================R"
+	def noticia
+		not_red = []
+		news = HTTParty.get("http://www.reddit.com/.json")
+		  news["data"]["children"].each do |notice|
+    	  title =  "#{notice["data"]["title"]}"
+    	  author =  "#{notice["data"]["author"]}"
+    	  link =  "#{notice["data"]["url"]}"
+    	  date =  Time.at(("#{notice["data"]["created"]}").to_i)
+        type = "Reddit"
+    	  notice = Notice.new(title, author, link, date, type)
+    	  not_red.push(notice)
+    		# "=================R"
 		end
+
+		return not_red
 	end	
 end	
 
-class Digg
+class Digg 
 	include HTTParty
 
-	def notices
- 		news = HTTParty.get("https://digg.com/api/news/popular.json?limit=5")
+	def noticia
+		not_digg = []
+ 		news = HTTParty.get("https://digg.com/api/news/popular.json")
     	news["data"]["feed"].each do |notice|
-   			puts "#{notice["content"]["title_alt"]}"  
-   			puts "#{notice["content"]["author"]}"
-   			puts "#{notice["content"]["url"]}"
-   			puts "#{notice["date_published"]}"
-   			puts "===================D"
+   		  title =  "#{notice["content"]["title_alt"]}"
+   		  author =  "#{notice["content"]["author"]}"
+   		  link =  "#{notice["content"]["url"]}"
+   		  date = Time.at(("#{notice["date_published"]}").to_i)
+        type = "Digg"
+   		  notice = Notice.new(title, author, link, date, type)
+   		  not_digg.push(notice)
+   			# "===================D"
 		end
+
+		return not_digg
 	end	
 end	
 
 def main
+	while true
 
-	noticias_mashable = Mashable.new
-	noticias_mashable.notices
-	noticias_reddit = Reddit.new
-	noticias_reddit.notices
-	noticias_digg = Digg.new
-	noticias_digg.notices
+    puts "Desea ver las noticias de que paginas"
+		puts "1. Mashable"
+		puts "2. Reddit"
+		puts "3. Digg"
+		puts "Indique el numero de la opcion"
+		sw = gets.to_i
+	
+		if sw != 1 && sw != 2 && sw != 3 
+			puts "opcion no valida"
+    end
+      
+		if sw == 1
+			m = Feed.new
+			aux = m.news("Mashable")
+			break
+		elsif sw == 2
+			r = Feed.new
+			aux = r.news("Reddit")
+			break
+		else
+			d = Feed.new
+			aux = d.news("Digg")
+      break
+    end				
 
+	end	
+
+  for i in 0...aux.length 
+    puts "===============================".colorize(:blue)
+	  puts aux[i].title
+    puts aux[i].author
+    puts aux[i].date
+    puts aux[i].link
+    puts aux[i].type
+  end
 end
 
 main
